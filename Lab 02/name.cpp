839453cd9f4a4a48d84d68390b50e1f957874c50
Duy Khoa <44254491+duykhoa999@users.noreply.github.com>
2018-10-18 14:19:03 +0700

    Update name.cpp

--- Lab 02/name.cpp
+++ Lab 02/name.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-  cout<<"*     *   *       *    * * *        * *";
-  cout<<"*   *     *       *  *       *     *   *";
-  cout<<"* *       *       *  *       *    *     *";
-  cout<<"* *       * * * * *  *       *   * * * * *";
-  cout<<"*   *     *       *  *       *  *         *";
-  cout<<"*    *    *       *    * * *   *           *";
+  cout<<"*     *   *       *    * * *        * *"<<endl;
+  cout<<"*   *     *       *  *       *     *   *"<<endl;
+  cout<<"* *       *       *  *       *    *     *"<<endl;
+  cout<<"* *       * * * * *  *       *   * * * * *"<<endl;
+  cout<<"*   *     *       *  *       *  *         *"<<endl;
+  cout<<"*    *    *       *    * * *   *           *"<<endl;
 }
